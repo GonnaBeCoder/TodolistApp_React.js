dce395e45352bb8d05dff459498fad15a013eb78
GonnaBeCoder <ch.badrinathreddy@gmail.com>
2025-09-04 00:42:55 +0530

    TO DO LIST APP IN REACTJS

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <p>This is my simple Project of To do app for general purpose . This is Done by Badrinath with the help of CodeWithHarry . This project has deepened my knowledge on props and routes and useEffect and components and DOM structure in ReactJS Framework . </p>
+    </div>
+  )
+}
+
+export default About
